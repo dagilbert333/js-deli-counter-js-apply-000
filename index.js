e6ea0ea89c782dead21a1ac3ddd6dd2fd678c57e
David Gilbert <dagilbert333@gmail.com>
2018-09-03 03:56:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ function nowServing(katzDeliLine){
 }
 
 function currentLine(katzDeliLine){
-  var i = 0;
+  var i = 0;ds
   return "The line is currently: 1. Ada, 2. Grace";
 }
+
+/ function currentLine(katzDeliLine) {
+//   var i = 0;
+//   while (i < katzDeli.length) {
+//     line.push(` `+[i+1]+`. `  + katzDeli[i])
+//     i++;
+//   }
+//   if (katzDeli.length === 0) {
+//     return "The line is currently empty.";
+//   } else
+//   return(`The line is currently:` + line);
+// }

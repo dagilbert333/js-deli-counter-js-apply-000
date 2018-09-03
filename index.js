7c858a6579fa25f50673e2da6422b2c1ea8ceead
David Gilbert <dagilbert333@gmail.com>
2018-09-03 02:34:08 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,10 @@
+function takeANumber(katzDeliLine, name){
+  
+  katzDeliLine.push(name);
+  
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
+}
+
+function nowServing(katzDeliLine){
+   katzDeliLine.shift();
+}

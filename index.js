2534ca6b914a24fd03d85b7d31974c8bcf3396a3
David Gilbert <dagilbert333@gmail.com>
2018-09-03 03:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function takeANumber(katzDeliLine, name){
 }
 
 function nowServing(katzDeliLine){
+   var i = 0;
    
    if (katzDeli.length === 0) {
     return "There is nobody waiting to be served!";
